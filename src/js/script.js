63d33874b7c867168314c54ff5227d1e21759a7f
azesmDotCom <azesm@yahoo.com>
2021-02-23 12:00:19 +0300

    11

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,16 @@ $(document).ready(function () {
         //adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
     });
 });
+
 
 
 //window.addEventListener('DOMContentLoaded', () => {
