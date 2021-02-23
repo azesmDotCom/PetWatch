359d175d1bef5884a4066d38d8802d7c241dad43
azesmDotCom <azesm@yahoo.com>
2021-02-23 21:12:56 +0300

    tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,42 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 1200,
-        //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [{
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
-            }
-        }]
-    });
+//<!------------------------------Any Sliders.css-------------------------->
+
+//    <!------------------------------Slick Slider.css-------------------------->
+
+//$(document).ready(function () {
+//    $('.carousel__inner').slick({
+//        speed: 1200,
+//        //adaptiveHeight: true,
+//        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//        responsive: [{
+//            breakpoint: 992,
+//            settings: {
+//                dots: true,
+//                arrows: false
+//            }
+//        }]
+//    });
+//});
+
+//<!------------------------------Tiny-Slider.css-------------------------->
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    axis: "vertical",
+    autoplay: false,
+    controls: false,
+    nav: false
 });
 
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
 
 
 //window.addEventListener('DOMContentLoaded', () => {
