361cbd296b0f9af86d55e658a654ed20639fa98b
azesmDotCom <azesm@yahoo.com>
2021-02-24 20:18:53 +0300

    не получилось сделать адаптив слик слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function () {
     $('.carousel__inner').slick({
-        speed: 1200,
+        speed: 500,
         //adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
@@ -13,10 +13,42 @@ $(document).ready(function () {
             settings: {
                 dots: true,
                 arrows: false
-            }
+            },
         }]
+        //slidesToShow: 4,
+        //slidesToScroll: 1,
+        //responsive: [{
+        //        breakpoint: 1024,
+        //        settings: {
+        //            slidesToShow: 3,
+        //            slidesToScroll: 3,
+        //            infinite: false,
+        //            dots: true
+        //        }
+        //    },
+        //    {
+        //        breakpoint: 992,
+        //        settings: {
+        //            slidesToShow: 1,
+        //            slidesToScroll: 1,
+        //            dots: false
+        //        }
+        //    },
+        //    {
+        //        breakpoint: 480,
+        //        settings: {
+        //            slidesToShow: 1,
+        //            slidesToScroll: 1
+        //        }
+        //    }
+        //    // You can unslick at a given breakpoint now by adding:
+        //    // settings: "unslick"
+        //    // instead of a settings object
+        //]
     });
 });
+
+
 
 //<!------------------------------Tiny-Slider.css-------------------------->
 //const slider = tns({
