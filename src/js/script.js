f069f9b5d63f407facba28c71950bcfa53502a33
azesmDotCom <azesm@yahoo.com>
2021-02-24 11:21:42 +0300

    bootstrapSlider-all slider

--- src/js/script.js
+++ src/js/script.js
@@ -19,24 +19,24 @@
 //});
 
 //<!------------------------------Tiny-Slider.css-------------------------->
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    axis: "vertical",
-    autoplay: false,
-    controls: false,
-    nav: false
-});
+//const slider = tns({
+//    container: '.carousel__inner',
+//    items: 1,
+//    slideBy: 'page',
+//    axis: "vertical",
+//    autoplay: false,
+//    controls: false,
+//    nav: false
+//});
 
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-});
+//document.querySelector('.prev').addEventListener('click', function () {
+//    slider.goTo('prev');
+//});
 
 
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-});
+//document.querySelector('.next').addEventListener('click', function () {
+//    slider.goTo('next');
+//});
 
 
 //window.addEventListener('DOMContentLoaded', () => {
