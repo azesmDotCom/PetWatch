b6db4b903dc7b74320ae259740d31fa79cbf40a6
azesmDotCom <azesm@yahoo.com>
2021-02-24 14:39:30 +0300

    slickSlider notResponsive

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,21 @@
 
 //    <!------------------------------Slick Slider.css-------------------------->
 
-//$(document).ready(function () {
-//    $('.carousel__inner').slick({
-//        speed: 1200,
-//        //adaptiveHeight: true,
-//        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-//        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-//        responsive: [{
-//            breakpoint: 992,
-//            settings: {
-//                dots: true,
-//                arrows: false
-//            }
-//        }]
-//    });
-//});
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+});
 
 //<!------------------------------Tiny-Slider.css-------------------------->
 //const slider = tns({
@@ -28,15 +28,13 @@
 //    controls: false,
 //    nav: false
 //});
-
 //document.querySelector('.prev').addEventListener('click', function () {
 //    slider.goTo('prev');
 //});
-
-
 //document.querySelector('.next').addEventListener('click', function () {
 //    slider.goTo('next');
 //});
+//<!---------------------------End of Tiny-Slider.js-------------------------->
 
 
 //window.addEventListener('DOMContentLoaded', () => {
