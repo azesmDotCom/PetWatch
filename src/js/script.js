0ea2d6078c41b39d9b02008ddccdd6ced1009350
azesmDotCom <azesm@yahoo.com>
2021-02-25 21:17:47 +0300

    tinySlider

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,24 @@
-//<!------------------------------Any Sliders.css-------------------------->
 
-//    <!------------------------------Slick Slider.css-------------------------->
 
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 500,
-        //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [{
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
-            },
-        }]
-        //slidesToShow: 4,
-        //slidesToScroll: 1,
-        //responsive: [{
-        //        breakpoint: 1024,
-        //        settings: {
-        //            slidesToShow: 3,
-        //            slidesToScroll: 3,
-        //            infinite: false,
-        //            dots: true
-        //        }
-        //    },
-        //    {
-        //        breakpoint: 992,
-        //        settings: {
-        //            slidesToShow: 1,
-        //            slidesToScroll: 1,
-        //            dots: false
-        //        }
-        //    },
-        //    {
-        //        breakpoint: 480,
-        //        settings: {
-        //            slidesToShow: 1,
-        //            slidesToScroll: 1
-        //        }
-        //    }
-        //    // You can unslick at a given breakpoint now by adding:
-        //    // settings: "unslick"
-        //    // instead of a settings object
-        //]
-    });
+
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    axis: "vertical",
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
 });
 
-
-
-//<!------------------------------Tiny-Slider.css-------------------------->
-//const slider = tns({
-//    container: '.carousel__inner',
-//    items: 1,
-//    slideBy: 'page',
-//    axis: "vertical",
-//    autoplay: false,
-//    controls: false,
-//    nav: false
-//});
-//document.querySelector('.prev').addEventListener('click', function () {
-//    slider.goTo('prev');
-//});
-//document.querySelector('.next').addEventListener('click', function () {
-//    slider.goTo('next');
-//});
-//<!---------------------------End of Tiny-Slider.js-------------------------->
 
 
 //window.addEventListener('DOMContentLoaded', () => {
