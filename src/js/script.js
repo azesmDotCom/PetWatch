2e682d5cab2883ba5a6a227e2fda935a5738a732
azesmDotCom <azesm@yahoo.com>
2021-03-11 18:31:30 +0300

    mailer work

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,27 @@
         $('input[name=phone]').mask("+7 (999) 999-99-99");
 
 
+
+        $('form').submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+
+
+
+
+                $('form').trigger('reset');
+            });
+            return false;
+        });
+
+
+
+
     });
 })(jQuery);
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'setforwebru@gmail.com';                 // Наш логин
+$mail->Password = '$Drog745#';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('setforwebru@gmail.com', 'azesmComPet');   // От кого письмо 
+$mail->addAddress('azesm@yahoo.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -30,7 +30,7 @@ $mail->isHTML(true);                                  // Set email format to HTM
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
-		Пользователь оставил данные <br> 
+    azesmComPet mail me setforwebru@gmail.com <br> 
 	Имя: ' . $name . ' <br>
 	Номер телефона: ' . $phone . '<br>
 	E-mail: ' . $email . '';
