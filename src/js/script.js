91a03c00ffec72721cc227330aa1084fb504c490
azesmDotCom <azesm@yahoo.com>
2021-03-12 13:12:59 +0300

    анимация css

--- src/js/script.js
+++ src/js/script.js
@@ -108,7 +108,22 @@
             return false;
         });
 
+        //scroll
 
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 1000) {
+                $('.pageup').fadeIn();
+            } else {
+                $('.pageup').fadeOut();
+            }
+        });
+        $("a[href^='#']").click(function () {
+            const _href = $(this).attr("href");
+            $("html, body").animate({
+                scrollTop: $(_href).offset().top + "px"
+            });
+            return false;
+        });
 
 
     });
